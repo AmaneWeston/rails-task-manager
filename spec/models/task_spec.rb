b249e0c85a8361979303a82b6a391c4386cb3621
Amane Weston <amane.weston02@gmail.com>
2022-03-29 17:07:01 +0900

    refactor

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,11 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  let(:task) { Task.new(title: 'Task', details: 'Very long details to test truncation') }
+  let!(:task) { Task.new(title: 'Task', details: 'Very long details to test truncation') }
 
   describe '#initialize' do
-    it 'returns a valid Task' do
-      expect(task.valid?).to eq (true)
+    context 'when valid' do
+      it 'returns a valid Task' do
+        expect(task.valid?).to eq(true)
+      end
+    end
+
+    context 'when invalid' do
+      context 'without title' do
+        before { task.title = nil }
+
+        it 'returns an invalid task' do
+          expect(task.valid?).to eq(false)
+        end
+
+        it 'returns an error message' do
+          task.valid?
+          expect(task.errors.messages).to eq({ title: ["can't be blank"]})
+        end
+      end
     end
   end
 
